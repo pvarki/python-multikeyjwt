293e9f38ae2546d30275e8c3a530388c192b3bab
Wille Ristimäki <villeristimaki@gmail.com>
2023-05-30 16:39:55 +0000

    test: add fixtures & tests for cli-commands

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,10 +1,13 @@
 """Test CLI scripts"""
 import asyncio
+import json
 
 import pytest
+
 from libadvian.binpackers import ensure_str
 
 from multikeyjwt import __version__
+from multikeyjwt.console import cli_group as cli
 
 
 @pytest.mark.asyncio
@@ -23,17 +26,16 @@ async def test_version_cli():  # type: ignore
     assert ensure_str(out[0]).strip().endswith(__version__)
 
 
-@pytest.mark.asyncio
-async def test_cli_output():  # type: ignore
-    """Run the entrypoint and check output"""
-    cmd = "multikeyjwt"
-    process = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    out = await asyncio.wait_for(process.communicate(), 10)
-    # Demand clean exit
-    assert process.returncode == 0
-    # Check output
-    assert ensure_str(out[0]).strip().endswith("Do your thing")
+def test_verify_cli(runner, token):  # type: ignore
+    """Test verify-command"""
+    result = runner.invoke(cli, ["verify", token.read_text(), "-k", "tests/data/rr_jwtRS256.pub"])
+
+    assert "foo" in json.loads(result.output)
+    assert result.exit_code == 0
+
+
+def test_sign_cli(runner):  # type: ignore
+    """Test sign-command"""
+    result = runner.invoke(cli, ["sign", "tests/data/rr_jwtRS256.key", "-p", "Rimmed_Radiated_Pliable_Perjury"])
+
+    assert result.exit_code == 0
